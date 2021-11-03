81d98cefc3cf0bbe168ec53f2eec85de1fdf2f97
mateus8489 <86335406+mateus8489@users.noreply.github.com>
2021-11-03 19:29:30 -0300

    Relacionamento

--- sysuser/src/main/java/br/com/res/domain/Categoria.java
+++ sysuser/src/main/java/br/com/res/domain/Categoria.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 
@@ -17,6 +18,9 @@ public class Categoria {
 	
 	private Integer id;
 	private String nome;
+	
+	@ManyToOne
+	private Produto produto;
 	
 	
 	@Override
--- sysuser/src/main/java/br/com/res/domain/Produto.java
+++ sysuser/src/main/java/br/com/res/domain/Produto.java
@@ -5,16 +5,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 
 @Entity
-
 public class Produto {
 	
 	 @Id
 	   @GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private String nome;
+	
+	@ManyToOne
+	private Categoria categoria;
+	
 	public Produto(Integer id, String nome) {
 		super();
 		this.id = id;
